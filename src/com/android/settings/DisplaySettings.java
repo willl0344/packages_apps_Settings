d1928a06a34800c98880d44b0a84f9d9b4f00236
Josue Rivera <prbassplayer@gmail.com>
2014-10-06 18:27:31 -0400

    Make "disable smart cover option" abide by smartcover overlay
    Change-Id: Ifb2987670f7a5ae5e27097c8cd036e56d5248836
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -119,6 +119,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private PackageStatusReceiver mPackageStatusReceiver;
     private IntentFilter mIntentFilter;
 
+    private int[] mSmartCoverCoords;
+
     private ContentObserver mAccelerometerRotationObserver =
             new ContentObserver(new Handler()) {
         @Override
@@ -206,12 +208,24 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 updateBatteryPulseDescription();
             }
 
+        mSmartCoverCoords = getResources().getIntArray(
+                com.android.internal.R.array.config_smartCoverWindowCoords);
+        if(mSmartCoverCoords.length != 4) {
+            // make sure there are exactly 4 dimensions provided, or ignore the values
+            mSmartCoverCoords = null;
+        }
+
         // Disable smart cover
          mDisableSmartCover = (CheckBoxPreference) findPreference(KEY_DISABLE_SMART_COVER);
+         PreferenceCategory wakeupOptions = (PreferenceCategory) findPreference("category_wakeup_options");
          if (mDisableSmartCover != null) {
-             mDisableSmartCover.setChecked(Settings.System.getInt(resolver,
-                     Settings.System.DISABLE_SMART_COVER, 0) == 1);
-             mDisableSmartCover.setOnPreferenceChangeListener(this);
+             if (mSmartCoverCoords == null) {
+                    wakeupOptions.removePreference(mDisableSmartCover);
+             } else {
+                    mDisableSmartCover.setChecked(Settings.System.getInt(resolver,
+                            Settings.System.DISABLE_SMART_COVER, 0) == 1);
+                    mDisableSmartCover.setOnPreferenceChangeListener(this);
+             }
          }
 
         // In-accurate proximity
